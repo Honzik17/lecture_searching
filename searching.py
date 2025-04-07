e983ac4afb5874d049cfe493b72f0a770da2b505
Honzik17 <jan1pecha@gmail.com>
2025-04-07 10:24:30 +0200

    tak ted jeste cas

--- searching.py
+++ searching.py
@@ -34,26 +34,49 @@ def linear_search(seq, hledane_cislo):
         idx += 1
     return {"position": ind, "count": count}
 
-def pattern_search(david, vzor):
+def pattern_search(david:str, sad:str):
     """
     :param david:
-    :param vzor:
+    :param sad:
     :return:
     """
-    int = []
-    i = 0
-    while i < len(david):
-        if david[i] == vzor:
-            int.append(i)
-        i += 1
-    return {"pozice": int}
+    pattern_length = len(sad)
+    sequence_length = len(david)
+    found_positions = set()
+
+    for i in range(sequence_length - pattern_length + 1):
+        if david[i:i + pattern_length] == sad:
+            found_positions.add(i)
+
+    return found_positions
+
+def binary_search(heh, number):
+    """
+    :param heh:
+    :param number:
+    :return:
+    """
+    kde_je = heh.index(number)
+    if number not in heh:
+        return None
+    return kde_je
 
 def main():
     file_name = "sequential.json"
+    sad = "ATA"
     #read data
     seq = read_data(file_name, field="unordered_numbers")
     david = read_data(file_name, field="dna_sequence")
+    heh = read_data(file_name, field="ordered_numbers")
     print(seq)
+    print(david)
+    print(heh)
+    hledane_cislo = 0
+    number = 25
+    result = linear_search(seq, hledane_cislo)
+    matches = pattern_search(david, sad)
+    number_pos = binary_search(heh, number)
+    return print(f"Hledané číslo jsme našli na indexu {result}. Patern jsme nalezli na indexu {matches}. Číslo se v posloupnosti nachází na místě {number_pos}.")
 
 if __name__ == '__main__':
     main()
