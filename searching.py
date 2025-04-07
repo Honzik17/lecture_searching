c4ade91ac77518b4d7fb1fe371e96e4a041dccc7
Honzik17 <jan1pecha@gmail.com>
2025-04-07 09:26:48 +0200

    dalsi vec

--- searching.py
+++ searching.py
@@ -18,10 +18,41 @@ def read_data(file_name, field):
         seq = json.load(json_file)
     return seq[field]
 
+def linear_search(seq, hledane_cislo):
+    """
+    :param seq:
+    :param hledane_cislo:
+    :return:
+    """
+    ind = []
+    count = 0
+    idx = 0
+    while idx < len(seq):
+        if seq[idx] == hledane_cislo:
+            ind.append(idx)
+            count += 1
+        idx += 1
+    return {"position": ind, "count": count}
+
+def pattern_search(david, vzor):
+    """
+    :param david:
+    :param vzor:
+    :return:
+    """
+    int = []
+    i = 0
+    while i < len(david):
+        if david[i] == vzor:
+            int.append(i)
+        i += 1
+    return {"pozice": int}
+
 def main():
     file_name = "sequential.json"
     #read data
     seq = read_data(file_name, field="unordered_numbers")
+    david = read_data(file_name, field="dna_sequence")
     print(seq)
 
 if __name__ == '__main__':
